cf508185b529bd69361e57b69b323b269b609578
sjcy2704 <a1834160@adelaide.edu.au>
2023-10-10 14:19:02 +1030

    Update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="px-20 py-2 text-right bg-base-100">
-	<p class="text-xs">© 2023 Derechos Reservados - Desarollado por sjcydev@DevPan</p>
+  <p class="text-xs">© 2023 Derechos Reservados - Desarollado por Ateliq</p>
 </footer>
